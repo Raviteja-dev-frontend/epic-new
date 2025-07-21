b4a6fc6fd5dfea992bd5f4d2d063c012c6eb87e3
Raviteja-dev-frontend <ravijobs1996@gmail.com>
2025-07-22 00:26:13 +0530

    Update vite.config.js vite

--- Ravi/frontend/vite.config.js
+++ Ravi/frontend/vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true,
+    allowedHosts: ['epic-new-frontend.onrender.com'] // 👈 Add this line
+  }
 })
