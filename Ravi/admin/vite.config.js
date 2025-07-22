5602dd599a34cee359a93717c2151fccf4064a7e
Raviteja-dev-frontend <ravijobs1996@gmail.com>
2025-07-22 23:01:29 +0530

    Update vite.config.js

--- Ravi/admin/vite.config.js
+++ Ravi/admin/vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173, // For local dev
-  },
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173, // For Render preview
-  },
+    port: parseInt(process.env.PORT) || 4173
+  }
 })
