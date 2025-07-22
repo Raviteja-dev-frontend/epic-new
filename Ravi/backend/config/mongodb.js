1d063c2dee715794327806e43a90eea465729c31
Raviteja-dev-frontend <ravijobs1996@gmail.com>
2025-07-23 00:57:09 +0530

    Update mongodb.js update

--- Ravi/backend/config/mongodb.js
+++ Ravi/backend/config/mongodb.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
-    mongoose.connection.on('connected',() => {
-        console.log("DB Connected");
-    })
+    // mongoose.connection.on('connected',() => {
+    //     console.log("DB Connected");
+    // })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err.message));
 
 }
 
